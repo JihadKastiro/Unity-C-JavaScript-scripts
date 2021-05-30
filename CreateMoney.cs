88d6badcebcf6be2b74388ee3fed0d5103152ad4
JihadKastiro <40108630+JihadKastiro@users.noreply.github.com>
2021-05-30 12:40:08 -0700

    Update CreateMoney.cs

--- CreateMoney.cs
+++ CreateMoney.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 
 public class CreateMoney : MonoBehaviour {
 	public  int getMoney;
-	//private MoneyScript Ms;
-
-	// Use this for initialization
+	
 	void Awake () {
-	//	Ms = GameObject.Find ("GameManager").GetComponent<MoneyScript> ();	
-	//	int myCoins = getMoney;
-	//	Ms.addCoins (getMoney);
-		//MoneyScript.moneys.addCoins (3);
+
 	}
 	
 	// Update is called once per frame
