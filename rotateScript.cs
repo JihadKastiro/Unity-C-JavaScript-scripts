ece5c0935c65ace8a6b71614cb8b1332ab0d649d
JihadKastiro <40108630+JihadKastiro@users.noreply.github.com>
2021-05-30 12:53:05 -0700

    Update rotateScript.cs

--- rotateScript.cs
+++ rotateScript.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 
 public class rotateScript : MonoBehaviour {
 	public  Transform tf;
-	//public float angle;
+	
 	public float xyz;
-	//public Vector3andSpace rotateDegreesPerSecond;
-	// Update is called once per frame
+	
 	void Update () {
 		Vector3 a = new Vector3 (0,xyz,0);
-		//tf.RotateAround (a,angle);
+		
 		tf.RotateAround(a,30);
 
-		//tf.rot
-		// transform.Rotate(rotateDegreesPerSecond.value*deltaTime, moveUnitsPerSecond.space);
 	}
 }
